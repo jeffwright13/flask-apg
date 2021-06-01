738e79f173393a2735ced3d2f50cc874e1786720
Jeff Wright <jeff.washcloth@gmail.com>
2021-06-01 19:26:03 -0400

    Rename upload dir. Enable logging.

--- app/__init__.py
+++ app/__init__.py
@@ -1,14 +1,17 @@
 from flask import Flask
 
-UPLOAD_FOLDER = r"/Users/Jeff/tmp"     # local folder to store uploaded files
-ALLOWED_EXTENSIONS = {'txt', "wav"}    # txt for phrase_file, wav for mix file
+FILE_FOLDER = r"/Users/jeff/tmp"  # local folder to store uploaded/generated files
+ALLOWED_EXTENSIONS = {"txt", "wav"}  # txt for phrase_file, wav for mix file
 MAX_CONTENT_LENGTH = 50 * 1024 * 1024  # max file upload file size ~ 50MB
 
 app = Flask(__name__)
 
-app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
-app.config['ALLOWED_EXTENSIONS'] = ALLOWED_EXTENSIONS
-app.config['MAX_CONTENT_LENGTH'] = MAX_CONTENT_LENGTH
+app.config["FILE_FOLDER"] = FILE_FOLDER
+app.config["ALLOWED_EXTENSIONS"] = ALLOWED_EXTENSIONS
+app.config["MAX_CONTENT_LENGTH"] = MAX_CONTENT_LENGTH
 
 from app import views
 from app import admin_views
+
+import logging
+logging.basicConfig(filename='flask.log', level=logging.DEBUG, format=f'%(asctime)s %(levelname)s %(name)s %(threadName)s : %(message)s')
