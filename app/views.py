add6ea18a2e85c29524238c86c350d2dc10bc1a2
Jeff Wright <jeff.washcloth@gmail.com>
2021-06-02 17:42:57 -0400

    Mimic same flow as with CLI version of APG.

--- app/views.py
+++ app/views.py
@@ -1,4 +1,3 @@
-import os
 from pathlib import Path
 
 from flask import (
@@ -14,71 +13,104 @@ from app import app
 import app.apg as apg
 
 
-def allowed_file(filename):
-    return (
-        "." in filename
-        and filename.rsplit(".", 1)[1].lower() in app.config["ALLOWED_EXTENSIONS"]
-    )
-
-
 @app.route("/setvals", methods=("GET", "POST"))
 def setvals():
 
     # All requests other than POST (i.e. GET) are re-shown the input form.
     if not request.method == "POST":
         return render_template("public/setvals2.html")
-    else:
-        to_mix = True if request.form.get("to_mix") == "on" else False
 
-        # Verify phrase_file type is allowed; if not, redirect to form page.
-        phrase_file = request.files["phrase_file"]
-        if not allowed_file(phrase_file.filename):
+    # Local handles for request object (type: Werkzeug FileStorage)
+    req_phrase_file_obj = request.files["phrase_file"]
+    req_sound_file_obj = request.files["sound_file"]
+
+    # Verify phrase_file type is allowed; if not, redirect to input form.
+    if (
+        Path(req_phrase_file_obj.filename).suffix
+        not in app.config["PHRASEFILE_EXTENSIONS"]
+    ):
+        return render_template("public/setvals.html")
+
+    # Save the phrase file (if non-null) to local file-upload dir
+    if req_phrase_file_obj.filename != "":
+        req_phrase_file_obj.save(
+            Path(app.config["FILE_FOLDER"])
+            / Path(secure_filename(req_phrase_file_obj.filename))
+        )
+        req_phrase_file_obj.close()
+
+    # Set fullpath local var to send to apg instance
+    phrase_file = (
+        Path(app.config["FILE_FOLDER"]) / Path(request.files["phrase_file"].filename)
+        if request.files["phrase_file"].filename != ""
+        else None
+    )
+
+    # Checkbox takes value "on" if enabled; null otherwise
+    to_mix = True if request.form.get("to_mix") == "on" else False
+
+    # Set rest of parameters if 'mix' option was selected
+    if to_mix:
+        attenuation = (
+            int(request.form.get("attenuation"))
+            if request.form.get("attenuation")
+            else 0
+        )
+
+        # Set fullpath local var to send to apg instance
+        sound_file = (
+            Path(app.config["FILE_FOLDER"]) / Path(request.files["sound_file"].filename)
+            if request.files["sound_file"].filename != ""
+            else None
+        )
+
+        # Verify sound_file type is allowed; if not, redirect to input form.
+        if (
+            Path(request.files["sound_file"].filename).suffix
+            not in app.config["SOUNDFILE_EXTENSIONS"]
+        ):
             return render_template("public/setvals.html")
-        else:
-            # Config app for file upload ocaion, and create dir if needed
-            Path(app.config["FILE_FOLDER"]).mkdir(parents=True, exist_ok=True)
 
-            # Save the phrase file to local file upload dir
-            phrase_savefile = secure_filename(phrase_file.filename)
-            phrase_file.save(
-                os.path.join(app.config["FILE_FOLDER"], phrase_savefile)
-            )  # CONVERT ME TO PATHLIB!
-            phrase_file.close()
-
-        if to_mix:
-            attenuation = (
-                int(request.form.get("attenuation"))
-                if request.form.get("attenuation")
-                else 0
+        # Save the sound file (if non-null) to local file-upload dir
+        req_sound_file_obj = request.files["sound_file"]
+        if req_sound_file_obj.filename != "":
+            req_sound_file_obj.save(
+                Path(app.config["FILE_FOLDER"])
+                / Path(secure_filename(req_sound_file_obj.filename))
             )
-            sound_file = request.files["sound_file"]
-            if not allowed_file(sound_file.filename):
-                return render_template("public/setvals.html")
-            else:
-                sound_savefile = secure_filename(sound_file.filename)
-                sound_file.save(
-                    os.path.join(app.config["FILE_FOLDER"], sound_savefile)
-                )  # CONVERT ME TO PATHLIB!
-                sound_file.close()
+            req_sound_file_obj.close()
 
-        # Instantiate Apg object with params passed in from HTML form
-        if to_mix:
-            A = apg.Apg(
-                os.path.join(app.config["FILE_FOLDER"], phrase_savefile),
-                to_mix,
-                os.path.join(app.config["FILE_FOLDER"], sound_savefile),
-                attenuation,
-            )  # CONVERT ME TO PATHLIB!
-        else:
-            A = apg.Apg(
-                os.path.join(app.config["FILE_FOLDER"], phrase_savefile)
-            )  # CONVERT ME TO PATHLIB!
+    # Instantiate Apg object with params passed in from HTML form
+    if to_mix:
+        A = apg.Apg(
+            phrase_file,
+            to_mix,
+            sound_file,
+            attenuation,
+        )
+    else:
+        A = apg.Apg(
+            phrase_file,
+        )
 
-        # Generate mixed sound file from speech, and serve in browser
-        A.invoke()
-        return redirect(url_for("get_file", path=Path(A.save_file).name))
+    # Generate mixed sound file from speech, then serve in browser
+    A.invoke()
+    return redirect(url_for("get_file", path=Path(A.save_file).name))
 
 
 @app.route("/get_file/<path:path>")
 def get_file(path):
     return send_from_directory(app.config["FILE_FOLDER"], path, as_attachment=True)
+
+
+def shutdown_server():
+    func = request.environ.get("werkzeug.server.shutdown")
+    if func is None:
+        raise RuntimeError("Not running with the Werkzeug Server")
+    func()
+
+
+@app.route("/shutdown", methods=["GET"])
+def shutdown():
+    shutdown_server()
+    return "Server shutting down..."
