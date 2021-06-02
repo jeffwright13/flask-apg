50ec839ace79eda8a3b62623cc85703251e80e23
Jeff Wright <jeff.washcloth@gmail.com>
2021-06-02 07:06:42 -0400

    Now implementing 'invoke()' method in apg class. Cleaned up comments. Attentuation is now defaulting to 0, even if form data is blank.

--- app/views.py
+++ app/views.py
@@ -5,13 +5,10 @@ from flask import (
     render_template,
     request,
     redirect,
-    jsonify,
     send_from_directory,
     url_for,
 )
 from werkzeug.utils import secure_filename
-
-from pydub import AudioSegment
 
 from app import app
 import app.apg as apg
@@ -26,10 +23,8 @@ def allowed_file(filename):
 
 @app.route("/setvals", methods=("GET", "POST"))
 def setvals():
-    # All requests other than POST (i.e. GET) are re-shown the input form.
-    # Data from the form's 'submit' button results in a POST, and will be
-    # processed by the audio_program_generator (apg) code.
 
+    # All requests other than POST (i.e. GET) are re-shown the input form.
     if not request.method == "POST":
         return render_template("public/setvals2.html")
     else:
@@ -40,7 +35,10 @@ def setvals():
         if not allowed_file(phrase_file.filename):
             return render_template("public/setvals.html")
         else:
+            # Config app for file upload ocaion, and create dir if needed
             Path(app.config["FILE_FOLDER"]).mkdir(parents=True, exist_ok=True)
+
+            # Save the phrase file to local file upload dir
             phrase_savefile = secure_filename(phrase_file.filename)
             phrase_file.save(
                 os.path.join(app.config["FILE_FOLDER"], phrase_savefile)
@@ -48,13 +46,19 @@ def setvals():
             phrase_file.close()
 
         if to_mix:
-            attenuation = int(request.form.get("attenuation"))
+            attenuation = (
+                int(request.form.get("attenuation"))
+                if request.form.get("attenuation")
+                else 0
+            )
             sound_file = request.files["sound_file"]
             if not allowed_file(sound_file.filename):
                 return render_template("public/setvals.html")
             else:
                 sound_savefile = secure_filename(sound_file.filename)
-                sound_file.save(os.path.join(app.config["FILE_FOLDER"], sound_savefile))  # CONVERT ME TO PATHLIB!
+                sound_file.save(
+                    os.path.join(app.config["FILE_FOLDER"], sound_savefile)
+                )  # CONVERT ME TO PATHLIB!
                 sound_file.close()
 
         if to_mix:
@@ -65,22 +69,18 @@ def setvals():
                 attenuation,
             )  # CONVERT ME TO PATHLIB!
         else:
-            A = apg.Apg(os.path.join(app.config["FILE_FOLDER"], phrase_savefile))  # CONVERT ME TO PATHLIB!
+            A = apg.Apg(
+                os.path.join(app.config["FILE_FOLDER"], phrase_savefile)
+            )  # CONVERT ME TO PATHLIB!
 
         A.gen_speech()
+        A.invoke()
 
-        # TODO: abstract out into the class?
-        if A.to_mix == True:
-            bkgnd = AudioSegment.from_file(A.sound_file, format="wav")
-            A.mix_file = A.mix(A.speech_file, bkgnd, A.attenuation)
-            A.mix_file.export(A.save_file, format="mp3")
-        else:
-            A.speech_file.export(A.save_file, format="mp3")
-        #breakpoint()
+        # Refresh page and direct broswer to save/open resultant file
+        # (see get_file endpoint directly below)
         return redirect(url_for("get_file", path=Path(A.save_file).name))
 
 
 @app.route("/get_file/<path:path>")
 def get_file(path):
-    """Download a file."""
     return send_from_directory(app.config["FILE_FOLDER"], path, as_attachment=True)
