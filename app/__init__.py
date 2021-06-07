49c9c0ad04d6a6278e5d1d26006abacb001764d7
Bob Belderbos <bobbelderbos@gmail.com>
2021-06-07 17:28:23 +0200

    load conf variable from environment

--- app/__init__.py
+++ app/__init__.py
@@ -1,17 +1,20 @@
-from flask import Flask
+import logging
+import os
 
-FILE_FOLDER = r"/Users/jeff/tmp"  # local folder to store uploaded/generated files
+from flask import Flask
+from dotenv import load_dotenv
+
+from app import views, admin_views
+
+load_dotenv()
+
 ALLOWED_EXTENSIONS = {"txt", "wav"}  # txt for phrase_file, wav for mix file
 MAX_CONTENT_LENGTH = 50 * 1024 * 1024  # max file upload file size ~ 50MB
 
 app = Flask(__name__)
 
-app.config["FILE_FOLDER"] = FILE_FOLDER
+app.config["FILE_FOLDER"] = os.getenv("APG_FILE_FOLDER")
 app.config["ALLOWED_EXTENSIONS"] = ALLOWED_EXTENSIONS
 app.config["MAX_CONTENT_LENGTH"] = MAX_CONTENT_LENGTH
 
-from app import views
-from app import admin_views
-
-import logging
 logging.basicConfig(filename='flask.log', level=logging.WARN, format=f'%(asctime)s %(levelname)s %(name)s %(threadName)s : %(message)s')
