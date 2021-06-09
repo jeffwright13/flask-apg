3222cbc82a80d093eff0d57f0d9b073642a84481
Bob Belderbos <bobbelderbos@gmail.com>
2021-06-09 16:31:48 +0200

    send logging to standard out if in debug mode, also added debug as explicit flag

--- app/__init__.py
+++ app/__init__.py
@@ -1,11 +1,14 @@
 import logging
 from pathlib import Path
 import os
+import sys
 
 from flask import Flask
 from dotenv import load_dotenv
 
 load_dotenv()
+
+DEBUG = os.getenv("DEBUG", False)
 
 
 def create_app():
@@ -13,6 +16,7 @@ def create_app():
     app.config["MAX_CONTENT_LENGTH"] = 50 * 1024 * 1024
     app.config["PHRASEFILE_EXTENSIONS"] = {".txt"}
     app.config["SOUNDFILE_EXTENSIONS"] = {".wav"}
+    app.config["DEBUG"] = DEBUG
     # TODO: remove this when the memory solution works
     app.config["FILE_FOLDER"] = os.getenv("APG_FILE_FOLDER")
     try:
@@ -22,8 +26,14 @@ def create_app():
     return app
 
 
-logging.basicConfig(
+logging_settings = dict(
     filename="flask.log",
     level=logging.WARN,
     format="%(asctime)s %(levelname)s %(name)s %(threadName)s : %(message)s",
 )
+if DEBUG:
+    # in debugging (local dev) mode, send logs to standard output
+    del logging_settings["filename"]
+    logging_settings["stream"] = sys.stdout
+
+logging.basicConfig(**logging_settings)
