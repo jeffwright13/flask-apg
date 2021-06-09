2b3ffbaea9d863283eb8b2bcb7d73c5e54b8cd63
Bob Belderbos <bobbelderbos@gmail.com>
2021-06-09 16:21:23 +0200

    made import redundant and followed app factory https://flask.palletsprojects.com/en/2.0.x/patterns/appfactories/

--- .flaskenv
+++ .flaskenv
@@ -1,2 +1,2 @@
-FLASK_APP=app
+FLASK_APP=app.views
 FLASK_ENV=development
--- app/__init__.py
+++ app/__init__.py
@@ -5,28 +5,25 @@ import os
 from flask import Flask
 from dotenv import load_dotenv
 
-# breakpoint()
-
-
 load_dotenv()
 
-app = Flask(__name__)
 
-from app import views, admin_views
+def create_app():
+    app = Flask(__name__)
+    app.config["MAX_CONTENT_LENGTH"] = 50 * 1024 * 1024
+    app.config["PHRASEFILE_EXTENSIONS"] = {".txt"}
+    app.config["SOUNDFILE_EXTENSIONS"] = {".wav"}
+    # TODO: remove this when the memory solution works
+    app.config["FILE_FOLDER"] = os.getenv("APG_FILE_FOLDER")
+    try:
+        Path(app.config["FILE_FOLDER"]).mkdir(parents=True, exist_ok=True)
+    except OSError:
+        pass
+    return app
 
-app.config["MAX_CONTENT_LENGTH"] = 50 * 1024 * 1024
-app.config["PHRASEFILE_EXTENSIONS"] = {".txt"}
-app.config["SOUNDFILE_EXTENSIONS"] = {".wav"}
-
-# TODO: remove this when the memory solution works
-app.config["FILE_FOLDER"] = os.getenv("APG_FILE_FOLDER")
-try:
-    Path(app.config["FILE_FOLDER"]).mkdir(parents=True, exist_ok=True)
-except OSError:
-    pass
 
 logging.basicConfig(
     filename="flask.log",
     level=logging.WARN,
-    format=f"%(asctime)s %(levelname)s %(name)s %(threadName)s : %(message)s",
+    format="%(asctime)s %(levelname)s %(name)s %(threadName)s : %(message)s",
 )
