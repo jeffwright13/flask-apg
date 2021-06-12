0e2b16c76306215cbb6d64e9a15cc2b8c61cac61
Bob Belderbos <bobbelderbos@gmail.com>
2021-06-12 09:42:07 +0200

    remove base64 stuff, this is something the lambda function does and the template renders the encoded file for us

--- app/utils.py
+++ app/utils.py
@@ -1,5 +1,4 @@
 import os
-from base64 import b64encode, b64decode
 
 import boto3
 from werkzeug.datastructures import FileStorage
@@ -28,14 +27,3 @@ def upload_to_s3(file: FileStorage, bucket=AWS_S3_BUCKET) -> str:
 
     return FILE_URL.format(bucket=bucket.name,
                            filename=filename)
-
-
-def encode_mp3_file(mp3_file):
-    with open(mp3_file, 'rb') as f:
-        return b64encode(f.read()).decode('utf-8')
-
-
-def decode_mp3_file(encoded_file, outfile="out.mp3"):
-    with open(outfile, 'wb') as f:
-        f.write(b64decode(encoded_file))
-    return outfile
