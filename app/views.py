9a358af628def5da75b7206feccca4adb08b8b60
Jeff Wright <jeff.washcloth@gmail.com>
2021-06-14 18:33:39 -0400

    Refurbish logic to conform with new **kwargs input format to apg.

--- app/views.py
+++ app/views.py
@@ -20,12 +20,12 @@ import apg
 app = create_app()
 
 
-@app.route("/setvals", methods=("GET", "POST"))
+@app.route("/", methods=("GET", "POST"))
 def setvals():
 
     # All requests other than POST (i.e. GET) are re-shown the input form.
     if not request.method == "POST":
-        return render_template("public/setvals2.html")
+        return render_template("public/setvals.html")
 
     # Local handles for request object (type: Werkzeug FileStorage)
     req_phrase_file_obj = request.files["phrase_file"]
@@ -37,12 +37,12 @@ def setvals():
         not in app.config["PHRASEFILE_EXTENSIONS"]
     ):
         return render_template("public/setvals.html")
-
+    """
     # Checkbox takes value "on" if enabled; null otherwise
-    to_mix = True if request.form.get("to_mix") == "on" else False
+    slow = True if request.form.get("slow") == "on" else False
 
     # Set rest of parameters if 'mix' option was selected
-    if to_mix:
+    if slow:
         attenuation = request.form.get("attenuation")
         attenuation = int(attenuation) if attenuation else 0
 
@@ -52,20 +52,19 @@ def setvals():
             not in app.config["SOUNDFILE_EXTENSIONS"]
         ):
             return render_template("public/setvals.html")
-
+    """
     # Instantiate AudioProgramGenerator object with params passed
     # in from HTML form
-    if to_mix:
-        p = StringIO(req_phrase_file_obj.read().decode())
-        s = BytesIO(req_sound_file_obj.read())
-        A = apg.AudioProgramGenerator(
-            p,
-            s,
-            attenuation,
-        )
-    else:
-        p = StringIO(req_phrase_file_obj.read().decode())
-        A = apg.AudioProgramGenerator(p)
+    slow = True if request.form.get("slow") == "on" else False
+    attenuation = request.form.get("attenuation")
+    kwargs = dict(slow=slow, attenuation=attenuation)
+    phr = StringIO(req_phrase_file_obj.read().decode())
+    snd = None if req_sound_file_obj.filename == '' else BytesIO(req_sound_file_obj.read())
+    A = apg.AudioProgramGenerator(
+        phr,
+        snd,
+        **kwargs,
+    )
 
     # Generate mixed sound file from speech, then serve in browser
     result = A.invoke()
@@ -75,14 +74,6 @@ def setvals():
         attachment_filename=str(Path(req_phrase_file_obj.filename)),
         as_attachment=True,
     )
-
-
-"""
-@app.route("/get_file/<path:path>")
-def get_file(path):
-    return send_from_directory(app.config["FILE_FOLDER"],
-                               path, as_attachment=True)
-"""
 
 
 def shutdown_server():
