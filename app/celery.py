a09c495b504eb1cbf6d423999a86de4446edc919
Bob Belderbos <bobbelderbos@gmail.com>
2021-08-30 22:44:56 +0200

    remove commented code

--- app/celery.py
+++ app/celery.py
@@ -7,10 +7,8 @@ def make_celery(app):
         broker=app.config['CELERY_BROKER_URL']
     )
     celery.conf.update(app.config)
-    # celery.conf.task_always_eager = True
     celery.conf.accept_content = ['pickle', 'application/json']
     celery.conf.task_serializer = 'pickle'
-    # celery.conf.result_serializer = 'pickle'
 
     class ContextTask(celery.Task):
         def __call__(self, *args, **kwargs):
