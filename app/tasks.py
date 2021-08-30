6453b8f5f8c7963b4ff613c0e3b5f2bebb403b28
Bob Belderbos <bobbelderbos@gmail.com>
2021-08-30 22:46:18 +0200

    flake8

--- app/tasks.py
+++ app/tasks.py
@@ -5,7 +5,9 @@ from dotenv import load_dotenv
 from app import app
 from app.celery import make_celery
 from app.aws import create_audio_mix as _create_audio_mix
-from app.results.db import create_db_and_tables, insert_file, update_file_with_result
+from app.results.db import (create_db_and_tables,
+                            insert_file,
+                            update_file_with_result)
 
 load_dotenv()
 
@@ -26,24 +28,21 @@ def create_audio_mix(
 ):
     # keep state in db
     file = insert_file(
-        req_phrase_filename,
-        req_sound_filename,
-        kwargs.get("attenuation", 10)
+        req_phrase_filename, req_sound_filename, kwargs.get("attenuation", 10)
     )
 
     # this takes long
-    phrase_file_path, sound_file_path, result_file_path, exception = _create_audio_mix(
-        req_phrase_filename,
-        req_phrase_file_content_encoded,
-        req_sound_filename,
-        req_sound_file_content_encoded,
-        **kwargs
+    phrase_file_path, sound_file_path, result_file_path, exception = (
+        _create_audio_mix(
+            req_phrase_filename,
+            req_phrase_file_content_encoded,
+            req_sound_filename,
+            req_sound_file_content_encoded,
+            **kwargs
+        )
     )
 
     # update db with results
     update_file_with_result(
-        file.id,
-        phrase_file_path,
-        sound_file_path,
-        result_file_path,
-        exception)
+        file.id, phrase_file_path, sound_file_path, result_file_path, exception
+    )
